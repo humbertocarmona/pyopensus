d99ca46ceceb4a622ce191a2ad0bebf6a98b0434
higorsmonteiro <higormonteiros@gmail.com>
2023-10-27 16:09:59 -0300

    first

---
+++ pyopensus/opensus.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+
+import os
+import ftplib
+import datetime as dt
+from ftplib import FTP 
+
+class Opensus:
+    '''
+    
+    '''
+    def __init__(self) -> None:
+        self._host = 'ftp.datasus.gov.br'
+        self.baseftp = FTP(self._host)
+        self.baseftp.login()
+        self.basepath = '/dissemin/publicos/'
+        
+        self.sys_included = {'sihsus': self.basepath+'SIHSUS/200801_/Dados/', 
+                             'siasus': self.basepath+'SIASUS/200801_/Dados/'}
+        
+    
+    # ---------------------------------------
+    # -- hide host string from the interface.
+    @property
+    def host(self):
+        return self.host
+    
+    @host.setter
+    def host(self, v):
+        raise Exception() # --
+        
+    def listdir(self, origin=None):
+        '''
+            List the files and folder of a specific directory within the host FTP.
+
+            Args:
+            -----
+                origin:
+                    String {default = None}. Name of the folder where to list the
+                    files. If None, lists the files in the home folder.
+        '''
+        if origin is None:
+            self.baseftp.cwd('/dissemin/publicos/')
+            self.baseftp.retrlines('LIST')
+        else:
+            if origin.lower() in self.sys_included.keys():
+                self.baseftp.cwd(self.sys_included[origin.lower()])
+                self.baseftp.retrlines('LIST')
+
+    def retrieve_year(self, dest, origin, uf, year, mode="RD", verbose=False):
+        '''
+            ...
+        '''
+        this_year = dt.date.today().year
+        if year < 2008 or year > this_year:
+            raise Exception() # --
+            
+        if origin.lower() not in self.sys_included.keys():
+            raise Exception() # --
+        else:
+            self.baseftp.cwd(self.sys_included[origin.lower()])
+            
+        year_str = f'{year}'[2:]
+        filename = f"{mode}{uf.upper()}{year_str}"
+        month_lst = ['01', '02', '03', '04', '05', '06', '07',
+                     '08', '09', '10', '11', '12']
+            
+        for cur_month in month_lst:
+            filename_ = f'{filename}{cur_month}.dbc'
+            if verbose:
+                print(f'Download do arquivo {filename_} ...', end='')
+                
+            with open(os.path.join(dest, filename_), 'wb') as fp:
+                self.baseftp.retrbinary(f'RETR {filename_}', fp.write)
+                
+            if verbose:
+                print(' Feito.')
